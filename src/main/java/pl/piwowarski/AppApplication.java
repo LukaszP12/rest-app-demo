ae69d93fef581326f4180322dc7e1c268ddee0c3
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-02 10:43:46 +0100

    added json convertion to java records for feign

--- src/main/java/pl/piwowarski/AppApplication.java
+++ src/main/java/pl/piwowarski/AppApplication.java
@@ -1,13 +1,38 @@
 package pl.piwowarski;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableFeignClients
 public class AppApplication {
+
+    @Autowired
+    private ShawnMendesProxy shawnMendesClient;
+
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void makeRequestToShawnMendesEndpoint() {
+        ShawnMendesResponse response = shawnMendesClient.makeSearchRequest("shawnmendes", 5);
+        System.out.println(response);
+        List<ShawnMendesResult> results = response.results();
+        ShawnMendesResult shawnMendesResult = results.get(0);
+        System.out.println(shawnMendesResult);
+        results.forEach(
+                result ->
+                        System.out.println(
+                                "shawnMendesResult.artistName(): " + result.artistName() +
+                                        "shawnMendesResult.trackName(): " + result.trackName()
+                        )
+        );
     }
 }
--- src/main/java/pl/piwowarski/ShawnMendesProxy.java
+++ src/main/java/pl/piwowarski/ShawnMendesProxy.java
@@ -2,10 +2,14 @@ package pl.piwowarski;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("shawnmendes-client")
+@FeignClient(value = "shawnmendes-client", url = "https://itunes.apple.com")
 public interface ShawnMendesProxy {
 
     @RequestMapping("/search")
-    String search();
+    ShawnMendesResponse makeSearchRequest(
+            @RequestParam("term") String term,
+            @RequestParam("limit") Integer limit
+    );
 }
